9e6dce72073b7b2da46370a72f596f8e90b78b26
SotofaSan <sotofasan@gmail.com>
2020-06-20 14:44:18 -0700

    4th homewok correction

--- features/steps/bestSellers.py
+++ features/steps/bestSellers.py
@@ -14,4 +14,4 @@ def open_amazon_best_sellers(context):
 @then('Verify the number of links is {number}')
 def verify_5_links_text(context, number):
     search_results = context.driver.find_elements(*RESULTS)
-    assert len(search_results) == int(number), "Expected number of links'{}', but got '{}'".format(number, search_results)
+    assert len(search_results) == int(number), "Expected number of links'{}', but got '{}'".format(number, len(search_results))
